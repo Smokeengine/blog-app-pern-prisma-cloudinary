4b07db32bc741b89bb7df09cba66dd085bc94f80
ANURAG VEMULA <101939604+Smokeengine@users.noreply.github.com>
2026-02-24 16:18:23 -0700

    Use environment variable for server port

--- blog_backend/app.js
+++ blog_backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('dotenv').config();
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 const cors = require('cors')
 
 app.use(express.json());
