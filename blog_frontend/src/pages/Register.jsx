90ffd28802ee736aab2441c22fcc2bc12b04c390
smokeengine <anuragvemula2240@gmail.com>
2026-02-24 16:35:44 -0700

    Fix: Remove unused response variable and add API config

--- blog_frontend/src/pages/Register.jsx
+++ blog_frontend/src/pages/Register.jsx
@@ -21,7 +21,7 @@ const Login = () => {
     e.preventDefault();
     try {
       setLoading(true);
-      const response = await axios.post(
+        await axios.post(
         "http://localhost:4000/api/user/register",
         {
             name,
