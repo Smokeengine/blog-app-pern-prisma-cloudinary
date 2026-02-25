8ea03c5b2ab27b4b701feb68ef8ee8223d96fdd5
ANURAG VEMULA <101939604+Smokeengine@users.noreply.github.com>
2026-02-24 22:21:22 -0700

    Update app.js

--- blog_backend/app.js
+++ blog_backend/app.js
@@ -14,7 +14,8 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const authRoutes = require('./src/routes/auth');
 const loginRoutes = require('./src/routes/login');
