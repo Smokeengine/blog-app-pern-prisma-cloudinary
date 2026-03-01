d2298528dccc93e9649c860d02b93965c3b59891
ANURAG VEMULA <101939604+Smokeengine@users.noreply.github.com>
2026-03-01 08:36:05 -0700

    Remove CORS options middleware
    Removed unnecessary CORS options middleware.

--- blog_backend/app.js
+++ blog_backend/app.js
@@ -17,7 +17,6 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.options('*', cors()); 
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
